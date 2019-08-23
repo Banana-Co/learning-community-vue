41206b257ff41c7ac4d95f4f8169d8f512868b2a
CastJo <34601111+CastJo@users.noreply.github.com>
2019-08-23 11:59:37 +0800

    update

--- src/components/forum.vue
+++ src/components/forum.vue
@@ -2,7 +2,7 @@
 	<div>
 		<div>测试</div>
 		<el-col :span="4"></el-col>
-		<el-col :span="16" :offset="4"><post></post>
+		<el-col :span="16" :offset="4">
 			<post></post>
 			<post></post>
 			<post></post>
@@ -10,21 +10,38 @@
 			<post></post>
 			<post></post>
 			<post></post>
-			<post></post></el-col>
-		
-		
-		
-			
-		
+			<post></post>
+			<post></post>
+			<el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page.sync="currentPage"
+			 :page-size="20" layout="prev, pager, next, jumper" :total="1000" :hide-on-single-page="true"> </el-pagination>
+		</el-col>
+
+
+
+
+
+
 	</div>
 </template>
 
 <script>
 	import post from "@/components/common/post.vue";
-	import navi from "@/components/common/navi.vue"
 	export default {
 		components: {
-			post,navi
+			post
+		},
+		methods: {
+			handleSizeChange(val) {
+				console.log(`每页 ${val} 条`);
+			},
+			handleCurrentChange(val) {
+				console.log(`当前页: ${val}`);
+			}
+		},
+		data() {
+			return {
+				currentPage: 1,
+			};
 		}
 	};
 </script>
