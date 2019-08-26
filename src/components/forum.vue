32f329d17a47c45f5ee05c8e51141479c0931db7
CastJo <34601111+CastJo@users.noreply.github.com>
2019-08-26 09:38:11 +0800

    整理代码格式

--- src/components/forum.vue
+++ src/components/forum.vue
@@ -20,24 +20,19 @@
 			</el-col>
 			<el-col :span="16">
 				<el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page.sync="currentPage"
-				 :page-size="10" layout="prev, pager, next, jumper" :total="totalPostNum" :hide-on-single-page="true"> </el-pagination>
+				 :page-size="10" layout="prev, pager, next, jumper" :total="totalPostNum" :hide-on-single-page="true">
+				</el-pagination>
 
 			</el-col>
 		</el-row>
 		<el-row>
 			<el-col :span="4">
-				<navi
-          @sort-change="handleSortChange">
-         </navi>
+				<navi @sort-change="handleSortChange">
+				</navi>
 			</el-col>
 			<el-col :span="16">
-				<outpost
-          v-for="post in posts"
-          :key="1"
-          :title="post.title"
-          :author="post.author"
-          :replyNum="post.replyNum"
-          :createdDate="post.createdDate"></outpost>
+				<outpost v-for="post in posts" :key="1" :title="post.title" :author="post.author" :replyNum="post.replyNum"
+				 :createdDate="post.createdDate"></outpost>
 			</el-col>
 		</el-row>
 	</div>
@@ -47,7 +42,11 @@
 	import outpost from "@/components/common/outPost.vue";
 	import navi from "@/components/common/navi.vue";
 	import PostDialog from "@/components/message/PostDialog.vue"
-	import {mapState,mapActions,mapGetters} from 'vuex';
+	import {
+		mapState,
+		mapActions,
+		mapGetters
+	} from 'vuex';
 	export default {
 		components: {
 			outpost,
@@ -56,7 +55,7 @@
 		},
 		computed: {
 			...mapState({
-				count1:state=>state.count
+				count1: state => state.count
 			})
 		},
 		methods: {
@@ -64,63 +63,55 @@
 				console.log(`每页 ${val} 条`);
 			},
 			handleCurrentChange(val) {
-			    this.getPostPage()
+				this.getPostPage()
 				console.log(`当前页: ${val}`);
 			},
-        handleSortChange(val) {
-			    this.sortedby = val.sortedby;
-			    this.order = val.order;
-			    this.currentPage = 1;
-			    this.getPostPage();
-        },
-			// addFun(){
-			// 	//this.$store.commit("add");
-			// 	this.$store.dispatch("addFun");
-			// },
-			// minusFun(){
-			// 	//this.$store.commit("reduction");
-			// 	this.$store.dispatch("reductionFun");
-			// },
+			handleSortChange(val) {
+				this.sortedby = val.sortedby;
+				this.order = val.order;
+				this.currentPage = 1;
+				this.getPostPage();
+			},
 			ToLogin() {
 				this.$router.replace({
 					path: '/login'
 				})
 			},
-        getPostPage() {
-            this.$axios
-                .get('http://localhost:8000/api/post/getPostByPage', {
-                    params: {
-                        page: this.currentPage,
-                        sortedby: this.sortedby,
-                        order: this.order
-                    }
-                })
-                .then(res => {
-                    this.posts = res.data.content;
-                    this.totalPostNum = res.data.totalElements;
-                })
-                .catch(function(error) {
-                    console.log(error);
-                })
-        },
-			showPost(){
-				this.postDialogVisible=true;
+			getPostPage() {
+				this.$axios
+					.get('http://localhost:8000/api/post/getPostByPage', {
+						params: {
+							page: this.currentPage,
+							sortedby: this.sortedby,
+							order: this.order
+						}
+					})
+					.then(res => {
+						this.posts = res.data.content;
+						this.totalPostNum = res.data.totalElements;
+					})
+					.catch(function(error) {
+						console.log(error);
+					})
+			},
+			showPost() {
+				this.postDialogVisible = true;
 			},
 		},
 		data() {
 			return {
 				currentPage: 1,
 				items: [require("@/assets/access.jpg"), require("@/assets/access1.jpg"), require("@/assets/default-8.png")],
-          postDialogVisible: false,
-          posts: [],
-          totalPostNum: 1,
-          sortedby: "createdDate",
-          order: "desc"
+				postDialogVisible: false,
+				posts: [],
+				totalPostNum: 1,
+				sortedby: "createdDate",
+				order: "desc"
 			};
 		},
-      created: function () {
-          this.getPostPage();
-      }
+		created: function() {
+			this.getPostPage();
+		}
 	};
 </script>
 
