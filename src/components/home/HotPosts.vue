cc9cba2604253b26f356a999116e4b665fc3ee49
CastJo <34601111+CastJo@users.noreply.github.com>
2019-08-31 21:11:59 +0800

    格式

--- src/components/home/HotPosts.vue
+++ src/components/home/HotPosts.vue
@@ -3,12 +3,12 @@
 		<el-card shadow="hover" class="threadBlock">
 			<el-row>公 告 区</el-row>
 			<el-table :data="announce" :show-header="false" @row-click="handleRowClick" style="width: 100%">
-				<el-table-column prop="title" label="标题" width="290">
+				<el-table-column prop="title" label="标题" width="260">
 				</el-table-column>
 			</el-table>
 			<el-row>当前热门 🔥</el-row>
 			<el-divider class = hotpostDivider></el-divider>
-			<el-table :data="post" :show-header="false" @row-click="handleRowClick" style="width: 100%">
+			<el-table :data="post" :show-header="false" @row-click="handleRowClick" style="width: 100%" height="200">
 				<el-table-column prop="title" label="标题" width="260">
 				</el-table-column>
 			</el-table>
