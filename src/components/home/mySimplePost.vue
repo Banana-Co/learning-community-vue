f738f8bd376c837a3188459f3a93f1daa6f49807
lyx <383625226@qq.com>
2019-08-31 12:06:57 +0800

    我的帖子简略页

--- src/components/home/mySimplePost.vue
+++ src/components/home/mySimplePost.vue
@@ -1,25 +1,22 @@
 <template>
 	<div>
 		<el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <el-row type="flex" justify="middle">
+          <el-col :span="4"> 我的帖子 </el-col>
+          <el-col :span="4" :offset="16"> <el-button size="mini" @click="toMyPost">查看全部</el-button> </el-col>
+        </el-row>
+      </div>
 			<div>
-				<el-table :data="this.notiData" style="width: 100%" max-height="250" ref="filterTable" :row-class-name="tableRowClassName"
-				 :default-sort="{prop: 'read', order: 'ascending'}">
-					<el-table-column prop="username" label="通知" width="100" sortable>
+				<el-table :data="this.post" style="width: 100%"
+                  :default-sort="{prop: 'createdDate', order: 'descending'}"
+                  @row-click="handleRowClick">
+					<el-table-column prop="title" label="标题" width="400">
 					</el-table-column>
-					<el-table-column prop="notifiDate" label="时间" width="200" :formatter="formatter" sortable>
+					<el-table-column prop="createdDate" label="时间" width="250" :formatter="formatter">
 					</el-table-column>
-					<el-table-column prop="message">
-					</el-table-column>
-					<el-table-column prop="read" label="类型" :filter-method="filterType" :filters="[{ text: '点赞', value: 1 }, { text: '评论', value: 2 }]"
-					 filter-placement="bottom-end">{{this.Read}}
-					</el-table-column>
-					<el-table-column fixed="right" label="操作" width="120">
-						<template slot-scope="scope">
-							<el-button @click.native.prevent="deleteRow(scope.$index, notiData)" type="text" size="small">
-								标为已读
-							</el-button>
-						</template>
-					</el-table-column>
+          <el-table-column prop="replyNum" label="回复数" width="100">
+          </el-table-column>
 				</el-table>
 			</div>
 		</el-card>
@@ -27,6 +24,28 @@
 </template>
 
 <script>
+  import {dateFormat} from "../../assets/js/time";
+
+  export default {
+      name: "mySimplePost",
+      data() {
+          return {
+
+          }
+      },
+      props: ['post'],
+      methods: {
+          formatter(row, column) {
+            return dateFormat(row.createdDate)
+          },
+          handleRowClick(row) {
+              this.$router.push(`/content/${row.id}`)
+          },
+          toMyPost() {
+              this.$router.push('/myPost')
+          }
+      }
+  }
 </script>
 
 <style>
